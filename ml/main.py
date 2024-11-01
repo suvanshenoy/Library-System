d725f41eceabc9a870d4fd052cef18bcd368f94f
suvanshenoy <suvanshenoy155@gmail.com>
2024-11-01 21:16:30 +0530

    unwanted python cache from ml directory

--- ml/main.py
+++ ml/main.py
@@ -1,17 +1,20 @@
 # venv: site-packages
 
 
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
+
 from library.recommendation import RecommendationSystem
 
 app = Flask(__name__)
 rec_sys = RecommendationSystem()
 
-@app.route('/recommendations', methods=['GET'])
+
+@app.route("/recommendations", methods=["GET"])
 def get_recommendations():
-    user_id = int(request.args.get('userId'))
+    user_id = int(request.args["userId"])
     recommendations = rec_sys.recommend(user_id)
     return jsonify(recommendations)
 
-if __name__ == '__main__':
-    app.run(host='localhost' port=5001)
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5001)
